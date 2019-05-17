1d80d8879e947d778dfa45e4ef9cb077f7a02cb9
tchiguel <tchiguel@p15>
2019-05-17 13:49:41 +0200

    Dec_to_Bin#2 all binary numbers on 4 bytes

--- Dec_to_Bin.py
+++ Dec_to_Bin.py
@@ -1,8 +1,11 @@
 def Dec_to_Bin(n):
-    tab_bin=[]
+    tab_bin=[0,0,0,0]
+    k=0
     i=n
-    while (i>1):
+    while (i>=1):
+
+        tab_bin[k]=i%2
         i=i//2
-        tab_bin.append(i%2)
+        k+=1
     tab_bin.reverse()
     return tab_bin

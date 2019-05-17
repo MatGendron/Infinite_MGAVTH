8d24195d118ebef0adb6836a84a97fbfe4cff4f9
Mathis Gendron <mathis.gendron@etu.u-bordeaux.fr>
2019-05-17 14:26:07 +0200

    Suppression spin.py, renommage de dec_to_bin en main et ajout de commentaires.

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def Dec_to_Bin(n):
     tab_bin.reverse()
     return tab_bin
 
+##Function that permutes two given values in an array
+
+##Function that takes an array of binaries and gives the corresponding decimal
+
+##Function that builds a 16x16 array of 0s 
+
 list_theta=[]
 for i in range(16):
     list_theta.append(Dec_to_Bin(i))
